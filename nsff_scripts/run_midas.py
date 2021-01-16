a1bb36aa99b08cddcc2708c7eefe19061f5c8a68
zhengqili <lixx921031@gmail.com>
2021-01-16 23:06:25 +0000

    script

---
+++ nsff_scripts/run_midas.py
@@ -0,0 +1,226 @@
+"""
+    Compute depth maps for images in the input folder.
+"""
+
+import os
+import glob
+import torch
+import utils
+import cv2
+import numpy as np
+from torchvision.transforms import Compose
+from models.midas_net import MidasNet
+from models.transforms import Resize, NormalizeImage, PrepareForNet
+
+import sys
+
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
+RESIZE_W_1, RESIZE_H_1 = 640, 360
+VIZ = False
+
+def _minify(basedir, factors=[], resolutions=[]):
+    '''
+        Minify the images to small resolution for training
+    '''
+
+    needtoload = False
+    for r in factors:
+        imgdir = os.path.join(basedir, 'images_{}'.format(r))
+        if not os.path.exists(imgdir):
+            needtoload = True
+    for r in resolutions:
+        imgdir = os.path.join(basedir, 'images_{}x{}'.format(r[1], r[0]))
+        if not os.path.exists(imgdir):
+            needtoload = True
+    if not needtoload:
+        return
+    
+    from shutil import copy
+    from subprocess import check_output
+    import glob
+
+    imgdir = os.path.join(basedir, 'images')
+    imgs = [os.path.join(imgdir, f) for f in sorted(os.listdir(imgdir))]
+    imgs = [f for f in imgs if any([f.endswith(ex) for ex in ['JPG', 'jpg', 'png', 'jpeg', 'PNG']])]
+    imgdir_orig = imgdir
+    
+    wd = os.getcwd()
+
+    for r in factors + resolutions:
+        if isinstance(r, int):
+            name = 'images_{}'.format(r)
+            resizearg = '{}%'.format(100./r)
+        else:
+            name = 'images_{}x{}'.format(r[1], r[0])
+            resizearg = '{}x{}'.format(r[1], r[0])
+
+        imgdir = os.path.join(basedir, name)
+        if os.path.exists(imgdir):
+            continue
+            
+        print('Minifying', r, basedir)
+        
+        os.makedirs(imgdir)
+        check_output('cp {}/* {}'.format(imgdir_orig, imgdir), shell=True)
+        
+        ext = imgs[0].split('.')[-1]
+        print(ext)
+        # sys.exit()
+        img_path_list = glob.glob(os.path.join(imgdir, '*.%s'%ext))
+        
+        for img_path in img_path_list:
+            save_path = img_path.replace('.jpg', '.png')
+            img = cv2.imread(img_path)
+
+            print(img.shape, r)
+            # sys.exit()
+
+            cv2.imwrite(save_path, cv2.resize(img, (r[1], r[0]), interpolation=cv2.INTER_AREA))
+
+        if ext != 'png':
+            check_output('rm {}/*.{}'.format(imgdir, ext), shell=True)
+            print('Removed duplicates')
+        print('Done')
+
+
+to8b = lambda x : (255*np.clip(x,0,1)).astype(np.uint8)
+import imageio
+
+def run(basedir, input_path, output_path, model_path, resize_height=288):
+    """Run MonoDepthNN to compute depth maps.
+
+    Args:
+        input_path (str): path to input folder
+        output_path (str): path to output folder
+        model_path (str): path to saved model
+    """
+    print("initialize")
+
+    img0 = [os.path.join(basedir, 'images', f) for f in sorted(os.listdir(os.path.join(basedir, 'images'))) \
+            if f.endswith('JPG') or f.endswith('jpg') or f.endswith('png')][0]
+    sh = cv2.imread(img0).shape
+    height = resize_height
+    factor = sh[0] / float(height)
+    width = int(round(sh[1] / factor))
+    _minify(basedir, resolutions=[[height, width]])
+
+    # select device
+    device = torch.device("cuda")
+    print("device: %s" % device)
+
+    small_img_dir = input_path + '_*x288/'
+    print(small_img_dir)
+
+    small_img_path = sorted(glob.glob(glob.glob(small_img_dir)[0] + '/*.png'))[0]
+
+    small_img = cv2.imread(small_img_path)
+
+    # load network
+    model = MidasNet(model_path, non_negative=True)
+
+    transform_1 = Compose(
+        [
+            Resize(
+                RESIZE_W_1,
+                RESIZE_H_1,
+                resize_target=None,
+                keep_aspect_ratio=True,
+                ensure_multiple_of=32,
+                resize_method="upper_bound",
+                image_interpolation_method=cv2.INTER_AREA,
+            ),
+            NormalizeImage(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+            PrepareForNet(),
+        ]
+    )
+
+    model.to(device)
+    model.eval()
+
+    # get input
+    img_names = sorted(glob.glob(os.path.join(input_path, "*")))
+    num_images = len(img_names)
+
+    # create output folder
+    os.makedirs(output_path, exist_ok=True)
+
+    print("start processing")
+
+    for ind in range(len(img_names)):
+
+        img_name = img_names[ind]
+        print("  processing {} ({}/{})".format(img_name, ind + 1, num_images))
+        # input
+        img = utils.read_image(img_name)
+        img_input_1 = transform_1({"image": img})["image"]
+
+        # compute
+        with torch.no_grad():
+            sample_1 = torch.from_numpy(img_input_1).to(device).unsqueeze(0)
+            prediction = model.forward(sample_1)
+            prediction = (
+                torch.nn.functional.interpolate(
+                    prediction.unsqueeze(1),
+                    size=[small_img.shape[0], 
+                        small_img.shape[1]],
+                    mode="nearest",
+                )
+                .squeeze()
+                .cpu()
+                .numpy()
+            )
+
+        # output
+        filename = os.path.join(
+            output_path, os.path.splitext(os.path.basename(img_name))[0]
+        )
+
+
+        if VIZ:
+            if not os.path.exists('./midas_otuputs'):
+                os.makedirs('./midas_otuputs')
+
+            plt.figure(figsize=(12, 6))
+            plt.subplot(1,2,1)
+            plt.imshow(img)
+            plt.subplot(1,2,2)
+            plt.imshow(prediction, cmap='jet') 
+            plt.savefig('./midas_otuputs/%s'%(img_name.split('/')[-1]))
+            plt.close()
+
+        print(filename + '.npy')
+        np.save(filename + '.npy', prediction.astype(np.float32))
+
+    print("finished")
+
+import argparse
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--data_path", type=str, 
+                        help='COLMAP Directory')
+    parser.add_argument("--resize_height", type=int, default=288,
+                        help='resized height for training images (width will be resized based on original aspect ratio), \
+                               default height 288')
+
+    args = parser.parse_args()
+    BASE_DIR = args.data_path
+
+    INPUT_PATH = BASE_DIR + "/images"
+    OUTPUT_PATH = BASE_DIR + "/disp"
+
+    MODEL_PATH = "model.pt"
+    if not os.path.exists(OUTPUT_PATH):
+        os.makedirs(OUTPUT_PATH)
+
+    # set torch options
+    torch.backends.cudnn.enabled = True
+    torch.backends.cudnn.benchmark = True
+
+    # compute depth maps
+    run(BASE_DIR, INPUT_PATH, OUTPUT_PATH, MODEL_PATH, args.resize_height)
+
+
---
+++ nsff_scripts/save_poses_nerf.py
@@ -0,0 +1,128 @@
+import colmap_read_model as read_model
+import numpy as np
+import os
+import sys
+
+def load_colmap_data(realdir):
+    
+    camerasfile = os.path.join(realdir, 'sparse/cameras.bin')
+    camdata = read_model.read_cameras_binary(camerasfile)
+    
+    list_of_keys = list(camdata.keys())
+    cam = camdata[list_of_keys[0]]
+    print( 'Cameras', len(cam))
+
+    h, w, f = cam.height, cam.width, cam.params[0]
+    # w, h, f = factor * w, factor * h, factor * f
+    hwf = np.array([h,w,f]).reshape([3,1])
+
+    imagesfile = os.path.join(realdir, 'sparse/images.bin')
+    imdata = read_model.read_images_binary(imagesfile)
+    
+    w2c_mats = []
+    bottom = np.array([0,0,0,1.]).reshape([1,4])
+    
+    names = [imdata[k].name for k in imdata]
+    img_keys = [k for k in imdata]
+
+    print( 'Images #', len(names))
+    perm = np.argsort(names)
+
+    points3dfile = os.path.join(realdir, 'sparse/points3D.bin')
+    pts3d = read_model.read_points3d_binary(points3dfile)
+
+    bounds_mats = []
+
+    upper_bound = 100
+
+    for i in perm[0:min(upper_bound, len(img_keys))]:
+        im = imdata[img_keys[i]]
+        print(im.name)
+        R = im.qvec2rotmat()
+        t = im.tvec.reshape([3,1])
+        m = np.concatenate([np.concatenate([R, t], 1), bottom], 0)
+        w2c_mats.append(m)
+
+        pts_3d_idx = im.point3D_ids
+        pts_3d_vis_idx = pts_3d_idx[pts_3d_idx >= 0]
+
+        # 
+        depth_list = []
+        for k in range(len(pts_3d_vis_idx)):
+          point_info = pts3d[pts_3d_vis_idx[k]]
+
+          P_g = point_info.xyz
+          P_c = np.dot(R, P_g.reshape(3, 1)) + t.reshape(3, 1)
+          depth_list.append(P_c[2])
+
+        zs = np.array(depth_list)
+        close_depth, inf_depth = np.percentile(zs, 5), np.percentile(zs, 95)
+        bounds = np.array([close_depth, inf_depth])
+        bounds_mats.append(bounds)
+
+    w2c_mats = np.stack(w2c_mats, 0)
+    # bounds_mats = np.stack(bounds_mats, 0)
+    c2w_mats = np.linalg.inv(w2c_mats)
+
+    poses = c2w_mats[:, :3, :4].transpose([1,2,0])
+    poses = np.concatenate([poses, np.tile(hwf[..., np.newaxis], [1,1,poses.shape[-1]])], 1)
+
+    # must switch to [-u, r, -t] from [r, -u, t], NOT [r, u, -t]
+    poses = np.concatenate([poses[:, 1:2, :], poses[:, 0:1, :], -poses[:, 2:3, :], poses[:, 3:4, :], poses[:, 4:5, :]], 1)
+    
+    save_arr = []
+
+    for i in range((poses.shape[2])):
+        save_arr.append(np.concatenate([poses[..., i].ravel(), bounds_mats[i]], 0))
+
+    save_arr = np.array(save_arr)
+    print(save_arr.shape)
+    np.save(os.path.join(realdir, 'poses_bounds.npy'), save_arr)
+
+
+def save_poses(basedir, poses, pts3d, perm):
+    pts_arr = []
+    vis_arr = []
+    for k in pts3d:
+        pts_arr.append(pts3d[k].xyz)
+        cams = [0] * poses.shape[-1]
+        for ind in pts3d[k].image_ids:
+            if len(cams) < ind - 1:
+                print('ERROR: the correct camera poses for current points cannot be accessed')
+                return
+            cams[ind-1] = 1
+        vis_arr.append(cams)
+
+    pts_arr = np.array(pts_arr)
+    vis_arr = np.array(vis_arr)
+    print( 'Points', pts_arr.shape, 'Visibility', vis_arr.shape )
+    
+    zvals = np.sum(-(pts_arr[:, np.newaxis, :].transpose([2,0,1]) - poses[:3, 3:4, :]) * poses[:3, 2:3, :], 0)
+    valid_z = zvals[vis_arr==1]
+    print( 'Depth stats', valid_z.min(), valid_z.max(), valid_z.mean() )
+    
+    save_arr = []
+    for i in perm:
+        vis = vis_arr[:, i]
+        zs = zvals[:, i]
+        zs = zs[vis==1]
+        close_depth, inf_depth = np.percentile(zs, 5), np.percentile(zs, 95)
+        print( i, close_depth, inf_depth )
+    
+        save_arr.append(np.concatenate([poses[..., i].ravel(), np.array([close_depth, inf_depth])], 0))
+    save_arr = np.array(save_arr)
+    
+    np.save(os.path.join(basedir, 'poses_bounds.npy'), save_arr)
+
+import argparse
+
+if __name__=='__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--data_path", type=str, 
+                        help='COLMAP Directory')
+
+    args = parser.parse_args()
+
+    basedir = args.data_path #"/phoenix/S7/zl548/nerf_data/%s/dense"%scene_name
+    load_colmap_data(basedir)
+    print( 'Done with imgs2poses' )
