8957dfa4579062686d6885de1211cf27b89cc262
zhengqili <zl548@phoenix28.cs.cornell.edu>
2021-07-13 20:21:20 -0400

    support 1000

--- nsff_scripts/save_poses_nerf.py
+++ nsff_scripts/save_poses_nerf.py
@@ -2,7 +2,26 @@ import colmap_read_model as read_model
 import numpy as np
 import os
 import sys
+import json
 
+def get_bbox_corners(points):
+  lower = points.min(axis=0)
+  upper = points.max(axis=0)
+  return np.stack([lower, upper])
+
+def filter_outlier_points(points, inner_percentile):
+  """Filters outlier points."""
+  outer = 1.0 - inner_percentile
+  lower = outer / 2.0
+  upper = 1.0 - lower
+  centers_min = np.quantile(points, lower, axis=0)
+  centers_max = np.quantile(points, upper, axis=0)
+  result = points.copy()
+
+  too_near = np.any(result < centers_min[None, :], axis=1)
+  too_far = np.any(result > centers_max[None, :], axis=1)
+
+  return result[~(too_near | too_far)]
 
 def load_colmap_data(realdir):
     camerasfile = os.path.join(realdir, 'sparse/cameras.bin')
@@ -14,7 +33,7 @@ def load_colmap_data(realdir):
 
     h, w, f = cam.height, cam.width, cam.params[0]
     # w, h, f = factor * w, factor * h, factor * f
-    hwf = np.array([h,w,f]).reshape([3,1])
+    hwf = np.array([h, w, f]).reshape([3,1])
 
     imagesfile = os.path.join(realdir, 'sparse/images.bin')
     imdata = read_model.read_images_binary(imagesfile)
@@ -31,9 +50,17 @@ def load_colmap_data(realdir):
     points3dfile = os.path.join(realdir, 'sparse/points3D.bin')
     pts3d = read_model.read_points3d_binary(points3dfile)
 
+    # extract point 3D xyz
+    point_cloud = []
+    for key in pts3d:
+        point_cloud.append(pts3d[key].xyz)
+
+    point_cloud = np.stack(point_cloud, 0)
+    point_cloud = filter_outlier_points(point_cloud, 0.95)
+
     bounds_mats = []
 
-    upper_bound = 100
+    upper_bound = 1000
     
     if upper_bound < len(img_keys):
         print("Only keeping " + str(upper_bound) + " images!")
@@ -53,7 +80,6 @@ def load_colmap_data(realdir):
         depth_list = []
         for k in range(len(pts_3d_vis_idx)):
           point_info = pts3d[pts_3d_vis_idx[k]]
-
           P_g = point_info.xyz
           P_c = np.dot(R, P_g.reshape(3, 1)) + t.reshape(3, 1)
           depth_list.append(P_c[2])
@@ -67,13 +93,23 @@ def load_colmap_data(realdir):
     # bounds_mats = np.stack(bounds_mats, 0)
     c2w_mats = np.linalg.inv(w2c_mats)
 
+    # bbox_corners = get_bbox_corners(point_cloud)
+    # also add camera 
+    bbox_corners = get_bbox_corners(
+                    np.concatenate([point_cloud, c2w_mats[:, :3, 3]], axis=0))
+    
+    scene_center = np.mean(bbox_corners, axis=0)
+    scene_scale = 1.0 / np.sqrt(np.sum((bbox_corners[1] - bbox_corners[0]) ** 2))
+
+    print('bbox_corners ', bbox_corners)
+    print('scene_center ', scene_center, scene_scale)
+
     poses = c2w_mats[:, :3, :4].transpose([1,2,0])
     poses = np.concatenate([poses, np.tile(hwf[..., np.newaxis], 
                                         [1,1,poses.shape[-1]])], 1)
 
-    # must switch to [-u, r, -t] from [r, -u, t], NOT [r, u, -t]
-    poses = np.concatenate([poses[:, 1:2, :], 
-                            poses[:, 0:1, :], 
+    # must switch to [-y, x, z] from [x, -y, -z], NOT [r, u, -t]
+    poses = np.concatenate([poses[:, 1:2, :], poses[:, 0:1, :], 
                             -poses[:, 2:3, :], 
                             poses[:, 3:4, :], 
                             poses[:, 4:5, :]], 1)
@@ -86,8 +122,13 @@ def load_colmap_data(realdir):
     save_arr = np.array(save_arr)
     print(save_arr.shape)
     np.save(os.path.join(realdir, 'poses_bounds.npy'), save_arr)
+    with open(os.path.join(realdir, 'scene.json'), 'w') as f:
+      json.dump({
+          'scale': scene_scale,
+          'center': scene_center.tolist(),
+          'bbox': bbox_corners.tolist(),
+      }, f, indent=2)
 
-    
 import argparse
 
 if __name__=='__main__':
